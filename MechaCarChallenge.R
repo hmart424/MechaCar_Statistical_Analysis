18bcb7fd78cded505e53e15ddfdac00a0da92347
hmart424 <95835840+hmart424@users.noreply.github.com>
2022-04-18 17:06:14 -0500

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,13 @@
+#Use the library() function to load the dplyr package.
+library(dplyr)
+
+#Import and read in the MechaCar_mpg.csv file as a dataframe.
+library(tidyverse)
+mecha_mpg <- read.csv(file='Desktop/Mecha_RStudio/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+#5. Perform linear regression using the lm() function
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)
+
+#Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg))
+
